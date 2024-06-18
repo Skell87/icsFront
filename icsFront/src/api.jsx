73d96038537cdf7b75c5c83c560e2bb25411a7c1
Skell87 <brandon.heisler@gmail.com>
2024-06-18 15:41:53 -0400

    changed url

--- icsFront/src/api.jsx
+++ icsFront/src/api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000";
+// const baseUrl = "http://127.0.0.1:8000";
+const baseUrl = "https://ics-back.fly.dev/";
 
 export const getToken = ({ username, password }) => {
   return axios
